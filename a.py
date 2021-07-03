d33c0de3a19b97a76165fc7eebe3726252a89e3e
Amaury941 <69680986+Amaury941@users.noreply.github.com>
2021-07-03 15:53:29 -0300

    Update a.py

--- a.py
+++ a.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-from scipy import stats
-import statistics as st
 import matplotlib.pyplot as plt
 
 n_classes = 7
@@ -35,12 +33,15 @@ data = {
   'Goiás':0, 
   'Distrito federal':0,
 }
-
-frequencias = {
-  '001-122':[0,0],'123-244':[0,0],'245-366':[0,0],
-  '367-488':[0,0],'489-610':[0,0],'611-732':[0,0],
-  '733-855':[0,0]
-}
+frequencias = [
+  ['001-122',0,0],
+  ['123-244',0,0],
+  ['245-366',0,0],
+  ['367-488',0,0],
+  ['489-610',0,0],
+  ['611-732',0,0],
+  ['733-855',0,0]
+]
 
 def calcular_data (x,data):
   for n in x['estado']:
@@ -53,48 +54,40 @@ def calcular_lista (data):
     lista.append(data[n])
   return lista
 
-def calcular_amplitude (maior,menor,n_classes):
-  amplitude = maior - menor
-  amplitude = round(amplitude/n_classes)
-  return amplitude
+def calcular_amplitude (maior,menor,n_classes): return round((maior - menor)/n_classes)
 
 def calcular_limites(menor,n_classes,amplitude_classe):
-  limites_inf = [menor]
+  limites = [menor]
   for i in range(n_classes):
-    limites_inf.append(limites_inf[i]+amplitude_classe)
-  return limites_inf
+    limites.append(limites[i]+amplitude_classe)
+  return limites
 
 def calcular_frequencia(frequencias,lista,limites_inf):
   for numero in lista:
-    if numero < limites_inf[1]:
-      frequencias['001-122'][0] +=1
-    elif numero < limites_inf[2]:
-      frequencias['123-244'][0] +=1
-    elif numero < limites_inf[3]:
-      frequencias['245-366'][0] +=1
-    elif numero < limites_inf[4]:
-      frequencias['367-488'][0] +=1
-    elif numero < limites_inf[5]:
-      frequencias['489-610'][0] +=1
-    elif numero < limites_inf[6]:
-      frequencias['611-732'][0] +=1
-    else:
-      frequencias['733-855'][0] +=1
+    limites_index = 1
+    zetr(numero,limites_inf,limites_index,frequencias)
   return frequencias
+
+def zetr (numero,limites,limites_index,frequencias):
+  if limites_index < len(limites):
+    if numero < limites[limites_index]:
+      frequencias[limites_index-1][1] += 1
+      return frequencias
+    limites_index += 1
+    zetr (numero,limites,limites_index,frequencias)
 
 def calcular_frequencia_relativa(lista,frequencias):
-  n_ocorrencias = len(lista)
   for classe in frequencias:
-    frequencias[classe][1] = 100*(frequencias[classe][0]/n_ocorrencias)
+    classe[2] = 100*(classe[1]/len(lista))
   return frequencias
 
+def calcular_moda(data,temp='',temp0=0):
+  for ds in data:
+    if data[ds] > temp0:
+      temp,temp0 = ds,data[ds]
+  return temp 
 
-
-def calcular_moda(lista):
-  return st.mode(lista)
-
-def calcular_media(lista):
-  return sum(lista)/len(lista)
+def calcular_media(lista): return sum(lista)/len(lista)
 
 def calcular_mediana(listaS):
     if len(listaS)%2 == 0:
@@ -103,8 +96,7 @@ def calcular_mediana(listaS):
       mediana = listaS[int(len(listaS)/2-1)]
     return mediana
 
-def calcular_variancia(lista):
-  var = 0
+def calcular_variancia(lista,var=0):
   media =calcular_media(lista)
   for numero in lista:
     var += (numero - media)**2
@@ -115,6 +107,7 @@ def calcular_desvio_padrão(lista):
     return calcular_variancia(lista)**(1/2)
 
 def mostrar_graficos (lista,data):
+
     plt.boxplot(lista)
     plt.title('Boxplot da Distribuição de cidades por unidade federativa')
     plt.xlabel('Número de cidades')
@@ -131,20 +124,26 @@ def mostrar_graficos (lista,data):
     ax.set_title('Distribuição de cidades por unidade federativa')
     plt.show()
 
+def mostrar_frequencias(frequencias):
+      print("\n|Classe |  Frequência  |")
+      for classe in frequencias:
+        print (
+          "|%s| %2i (%s%s) |" % 
+          (classe[0],classe[1],
+          (("%2i"%classe[2])+
+          (
+            "%.3f"%(classe[2]-
+            int("%2i"%classe[2])))[1:]
+          ),
+          "%")
+        )
+
 def quicksort(arr):
     if len(arr) <= 1: return arr
     m = arr[0]
     return quicksort([i for i in arr if i < m]) + \
         [i for i in arr if i == m] + \
         quicksort([i for i in arr if i > m])
-
-def printar_coisas(lista,listaS,frequencias,menor,q1,q2,q3,maior):
-    print ('| Média: %.2f | Mediana: %.2f | Moda: %.2f |' % (calcular_media(lista),calcular_mediana(listaS),calcular_moda(lista)) )
-    print ('| Variância: %.2f | Desvio padrão: %.2f |' % (calcular_variancia(lista),calcular_desvio_padrão(lista)))
-    print ('| Mínimo: %s | Q1: %s | Q2: %s | Q3: %s | Máximo: %s |' % (menor,q1,q2,q3,maior))
-    print("\n|Classe |  Frequência  |")
-    for n in frequencias:
-        print ("|%s| %2i (%s%s) |" % (n,frequencias[n][0],(("%2i"%frequencias[n][1])+("%.3f"%(frequencias[n][1]-int("%2i"%frequencias[n][1])))[1:]),"%"))
 
 def main (n_classes, x, data, frequencias):
     data             = calcular_data (x,data)
@@ -153,15 +152,26 @@ def main (n_classes, x, data, frequencias):
     maior            = max(lista)
     menor            = min(lista)
     amplitude_classe = calcular_amplitude (maior,menor,n_classes)
-    limites_inf      = calcular_limites(menor,n_classes,amplitude_classe)
-    frequencias      = calcular_frequencia(frequencias,lista,limites_inf)
+    limites          = calcular_limites(menor,n_classes,amplitude_classe)
+    frequencias      = calcular_frequencia(frequencias,lista,limites)
     frequencias      = calcular_frequencia_relativa(lista,frequencias)
 
     q2 = (calcular_mediana(listaS))
     q1 = (calcular_mediana(listaS[0:listaS.index(q2)]))
     q3 = (calcular_mediana(listaS[listaS.index(q2):]))
 
-    printar_coisas(lista,listaS,frequencias,menor,q1,q2,q3,maior)
-    #mostrar_graficos (lista,data)
+    print (
+      '\nMédia de cidades: %.2f\nMediana de cidades: %.2f\nModa de estados: %s\nVariância: %.2f\nDesvio padrão: %.2f' 
+      %(
+        calcular_media(lista),
+        calcular_mediana(listaS),
+        calcular_moda(data),
+        calcular_variancia(lista),
+        calcular_desvio_padrão(lista)
+      )
+    )
+    print ('\n| Mínimo: %s | Q1: %s | Q2: %s | Q3: %s | Máximo: %s |' % (menor,q1,q2,q3,maior))
+    mostrar_frequencias(frequencias)
+    mostrar_graficos (lista,data)
 
 main (n_classes, x, data, frequencias)
